f8d7170cf9eb9ce7a5f1dd7ab5821f91b93b1ae6
Dan McGrath <daniel.mcgrath9@gmail.com>
2018-02-16 09:40:54 -0500

    add product service, bind click event to star component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { ProductService } from './product.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ProductService]
 })
 export class AppComponent {
   title = 'Acme Product Management';
